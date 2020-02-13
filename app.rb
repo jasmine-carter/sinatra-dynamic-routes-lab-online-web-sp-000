dba6226e76272a6504b64b9ed33ca41b70ff31da
Jasmine Carter <jasmine.carter87@gmail.com>
2020-02-13 02:30:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,7 +26,7 @@ class App < Sinatra::Base
     "#{answer << "."}"
   end
 
-  get "/:operation/:number1/:number2"
+  get "/:operation/:number1/:number2" do
     if param[:operation] == "add"
       "#{param[:number1].to_i + param[:number2].to_i}"
     elsif param[:operation] == "subtract"
